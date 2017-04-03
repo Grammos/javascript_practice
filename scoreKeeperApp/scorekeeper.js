3dfe907978627a05030fc46fa5bb44b44bbae8ff
Grammos <kamberigramos@gmail.com>
2017-04-03 15:31:17 +0200

    Adding a display score to the 'Playing to:___'

--- scoreKeeperApp/scorekeeper.js
+++ scoreKeeperApp/scorekeeper.js
@@ -3,6 +3,9 @@ var p2Button = document.getElementById("p2");
 var resetButton = document.getElementById("reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var numInput = document.querySelector("input");
+var p = document.querySelector("p");
+var winningScoreDisplay = document.querySelector("p span");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
@@ -40,6 +43,7 @@ p2Button.addEventListener("click", function(){
 });
 
 
+
 //Reset button
 
 resetButton.addEventListener("click", function(){
@@ -56,4 +60,14 @@ resetButton.addEventListener("click", function(){
 	gameOver = false;
 
 });
+
+//everytime the winning score change,
+
+numInput.addEventListener("change", function(){
+	//added the numbers to the MSG of the 'Playin to: the users' input
+  winningScoreDisplay.textContent = numInput.value;
+  winningScore = Number(numInput.value);
+
+});
+
 
