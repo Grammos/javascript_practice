92f9fb41b6529d5db0ec166b349df5d4f6cba91c
Grammos <kamberigramos@gmail.com>
2017-04-03 14:37:49 +0200

    added a css class to the 1 and 2 buttons, 4 announ. the winner with a color.

--- scoreKeeperApp/scorekeeper.js
+++ scoreKeeperApp/scorekeeper.js
@@ -16,6 +16,7 @@ p1Button.addEventListener("click", function(){
    if(!gameOver){
 	 p1Score++;
 	 if(p1Score === winningScore){
+	 	p1Display.classList.add("winner");
 	 	gameOver = true;
 	 }
 
@@ -29,6 +30,7 @@ p2Button.addEventListener("click", function(){
   if(!gameOver){
   	p2Score++;
   	if(p2Score === winningScore){
+  	   p2Display.classList.add("winner")
   		gameOver = true;
   	}
   }
