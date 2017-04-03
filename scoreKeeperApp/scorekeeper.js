0e77cd15dd10919792c1e375e38f472744825a7d
Grammos <kamberigramos@gmail.com>
2017-04-03 14:14:23 +0200

    Added the limit of players to a 'winning state' that would be number 5!

--- scoreKeeperApp/scorekeeper.js
+++ scoreKeeperApp/scorekeeper.js
@@ -4,6 +4,8 @@ var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
 var p1Score = 0;
 var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
 
 
 
@@ -11,17 +13,25 @@ var p2Score = 0;
 //Players One button
 
 p1Button.addEventListener("click", function(){
+   if(!gameOver){
+	 p1Score++;
+	 if(p1Score === winningScore){
+	 	gameOver = true;
+	 }
 
-  p1Score++;
-  p1Display.textContent = p1Score;
-
+	 p1Display.textContent = p1Score;
+   }
 });
 
 //Player Two button
 
 p2Button.addEventListener("click", function(){
-
-   p2Score++;
+  if(!gameOver){
+  	p2Score++;
+  	if(p2Score === winningScore){
+  		gameOver = true;
+  	}
+  }
    p2Display.textContent = p2Score;
 
 });
