abee721a24cfafd56ee21d7aaadbb95f07f88bdc
Grammos <kamberigramos@gmail.com>
2017-04-03 13:29:16 +0200

    added the testing addEventListener to the first button

--- scoreKeeperApp/scorekeeper.js
+++ scoreKeeperApp/scorekeeper.js
@@ -1,1 +1,10 @@
-alert("Hello");
+var p1button = document.querySelector("#p1");
+var p2button = document.getElementById("p2");
+
+p1button.addEventListener("click", function(){
+
+	alert("Clicked");
+
+});
+
+
