8f78791a1b97f994c8abbf574acf792c45e8de11
Grammos <kamberigramos@gmail.com>
2017-04-03 14:04:51 +0200

    Functionalized the 2 first buttons.

--- scoreKeeperApp/scorekeeper.js
+++ scoreKeeperApp/scorekeeper.js
@@ -1,9 +1,28 @@
-var p1button = document.querySelector("#p1");
-var p2button = document.getElementById("p2");
+var p1Button = document.querySelector("#p1");
+var p2Button = document.getElementById("p2");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
+var p1Score = 0;
+var p2Score = 0;
 
-p1button.addEventListener("click", function(){
 
-	alert("Clicked");
+
+
+//Players One button
+
+p1Button.addEventListener("click", function(){
+
+  p1Score++;
+  p1Display.textContent = p1Score;
+
+});
+
+//Player Two button
+
+p2Button.addEventListener("click", function(){
+
+   p2Score++;
+   p2Display.textContent = p2Score;
 
 });
 
