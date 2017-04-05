7fa42c96efaa4f156e474cff641d6efc01d533ae
Grammos <kamberigramos@gmail.com>
2017-04-05 23:00:56 +0200

    Added the fadeOut effect to the span's X!

--- toDos_app/assets/js/todos.js
+++ toDos_app/assets/js/todos.js
@@ -2,3 +2,15 @@
 $("li").click(function(){
   $(this).toggleClass("completed");	
 });
+
+//Click on X to delete Todos
+$("span").click(function(event){
+  $(this).parent().fadeOut(500, function(){
+  	//delete the li not the span!
+  	$(this).remove();
+  });
+ //it would fire to the span but not continue on the other events!
+ //so would stop the "bubble up" effect.
+  event.stopPropagation();
+});
+
