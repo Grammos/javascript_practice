f098b2022795224e5434210a9e47708223c2a83a
Grammos <kamberigramos@gmail.com>
2017-04-05 23:57:38 +0200

    on(), append() methods.

--- toDos_app/assets/js/todos.js
+++ toDos_app/assets/js/todos.js
@@ -1,10 +1,10 @@
 // Check off specific Todos by clicking 
-$("li").click(function(){
+$("ul").on("click", "li",  function(){
   $(this).toggleClass("completed");	
 });
 
 //Click on X to delete Todos
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
   $(this).parent().fadeOut(500, function(){
   	//delete the li not the span!
   	$(this).remove();
@@ -12,5 +12,15 @@ $("span").click(function(event){
  //it would fire to the span but not continue on the other events!
  //so would stop the "bubble up" effect.
   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13){
+  	//grabbing new todo text from input
+     var todoText = $(this).val();
+     $(this).val("");
+     //create a new li and add to ul
+     $("ul").append("<li><span>X</span> " + todoText + "</li>");
+  }
 });
 
